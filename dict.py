3fc284c551e6261afb932505fe6242a5f83c461e
Ajaikrishnan M <ajai2362007@gmail.com>
2026-08-20 12:00:31 +0530

    Delete dict.py

--- dict.py
+++
@@ -1,9 +0,0 @@
-text = input("Enter a text: ")
-words = text.lower().split()
-word_count = {}
-for word in words:
-    if word in word_count:
-        word_count[word] += 1
-    else:
-        word_count[word] = 1
-print(word_count)
